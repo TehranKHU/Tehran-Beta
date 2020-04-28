7a6d3c2880e09d758e57cfde7675015816bc9e75
BijanProgrammer <r6jesuschrist@gmail.com>
2020-04-28 05:09:36 +0430

    Added private GET method to check if user is logged in

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,6 +12,8 @@ function auth(req, res, next) {
 	}
 
 	try {
+		console.log('--> token --> ', token);
+
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
 		req.user = decoded;
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const config = require('config');
+const authMiddleware = require('../../middleware/auth');
 
 const database = require('./database');
 const errorHandler = require('../../helpers/error_handler');
@@ -19,7 +20,7 @@ router.post('/', (req, res) => {
 		return res.status(400).json({ msg: 'لطفاً تمام فیلدها را پر کنید.' });
 	}
 
-	// Check for existing model
+	// Check for existing user
 	let command = `SELECT * FROM user WHERE username="${username}"`;
 
 	database.query(command, (err, result) => {
@@ -51,6 +52,18 @@ router.post('/', (req, res) => {
 				);
 			});
 		}
+	});
+});
+
+// @route	GET		api/auth/user
+// @desc	Auth User
+// @acess	Private
+router.get('/user', authMiddleware, (req, res) => {
+	let command = `SELECT * FROM user WHERE username="${req.user.username}"`;
+
+	database.query(command, (err, result) => {
+		delete result[0].password;
+		res.json(result[0]);
 	});
 });
 
