c51474fc5033a18e5e7826cf81d191e1a167b48a
BijanProgrammer <r6jesuschrist@gmail.com>
2020-04-28 04:51:13 +0430

    Created auth.js middleware to check whether or not user is logged in

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+	const token = req.header('x-auth-token');
+
+	// Check token
+	if (!token) {
+		return res
+			.status(401)
+			.json({ msg: 'شما اجازه‌ی دسترسی به این بخش را ندارید.' });
+	}
+
+	try {
+		const decoded = jwt.verify(token, config.get('jwtSecret'));
+		req.user = decoded;
+
+		next();
+	} catch (e) {
+		res.status(400).json({ msg: 'توکن معتبر نیست.' });
+	}
+}
+
+module.exports = auth;
